c78fa9b22638ccfbbdcb75dda8fd858566d955c9
unknown <emyokalla@gmail.com>
2024-03-02 17:47:08 +0100

    Integrating menu item features with pages and initializing cart functionality

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -1,5 +1,5 @@
 'use client';
-// import {CartContext} from "@/components/AppContext";
+import {CartContext} from "../../components/AppContext";
 import Bars2 from "../../components/icons/Bars2";
 import ShoppingCart from "../../components/icons/ShoppingCart";
 import {signOut, useSession} from "next-auth/react";
@@ -39,7 +39,7 @@ export default function Header() {
   const status = session?.status;
   const userData = session.data?.user;
   let userName = userData?.name || userData?.email;
-  // const {cartProducts} = useContext(CartContext);
+  const {cartProducts} = useContext(CartContext);
   const [mobileNavOpen, setMobileNavOpen] = useState(false);
   if (userName && userName.includes(' ')) {
     userName = userName.split(' ')[0];
@@ -53,11 +53,11 @@ export default function Header() {
         <div className="flex gap-8 items-center">
           <Link href={'/cart'} className="relative">
             <ShoppingCart />
-            {/* {cartProducts?.length > 0 && (
+            {cartProducts?.length > 0 && (
               <span className="absolute -top-2 -right-4 bg-primary text-white text-xs py-1 px-1 rounded-full leading-3">
             {cartProducts.length}
           </span>
-            )} */}
+            )}
           </Link>
           <button
             className="p-1 border"
@@ -91,11 +91,11 @@ export default function Header() {
           <AuthLinks status={status} userName={userName} />
           <Link href={'/cart'} className="relative">
             <ShoppingCart />
-            {/* {cartProducts?.length > 0 && (
+            {cartProducts?.length > 0 && (
               <span className="absolute -top-2 -right-4 bg-primary text-white text-xs py-1 px-1 rounded-full leading-3">
             {cartProducts.length}
           </span>
-            )} */}
+            )}
           </Link>
         </nav>
       </div>
--- src/components/layout/HomeMenu.js
+++ src/components/layout/HomeMenu.js
@@ -1,18 +1,18 @@
 'use client';
 import SectionHeaders from "../../components/layout/SectionHeaders";
-// import MenuItem from "@/components/menu/MenuItem";
+import MenuItem from "../../components/menu/MenuItem";
 import Image from "next/image";
 import {useEffect, useState} from "react";
 
 export default function HomeMenu() {
   const [bestSellers, setBestSellers] = useState([]);
-  // useEffect(() => {
-  //   fetch('/api/menu-items').then(res => {
-  //     res.json().then(menuItems => {
-  //       setBestSellers(menuItems.slice(-3));
-  //     });
-  //   });
-  // }, []);
+  useEffect(() => {
+    fetch('/api/menu-items').then(res => {
+      res.json().then(menuItems => {
+        setBestSellers(menuItems.slice(-3));
+      });
+    });
+  }, []);
   return (
     <section className="">
       <div className="absolute left-0 right-0 w-full justify-start">
@@ -29,9 +29,9 @@ export default function HomeMenu() {
           mainHeader={'Our Best Sellers'} />
       </div>
       <div className="grid sm:grid-cols-3 gap-4">
-        {/* {bestSellers?.length > 0 && bestSellers.map(item => (
+        {bestSellers?.length > 0 && bestSellers.map(item => (
           <MenuItem key={item._id} {...item} />
-        ))} */}
+        ))}
       </div>
     </section>
   );
